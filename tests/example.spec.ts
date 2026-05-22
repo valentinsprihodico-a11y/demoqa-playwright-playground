16e25147be43b3ed89d554c317a4b20ea77e2826
valentinsprihodico-a11y <valentins.prihodico@gmail.com>
2026-05-22 14:59:04 +0300

    empty commit agait

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,5 +16,3 @@ test.skip('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-
-
